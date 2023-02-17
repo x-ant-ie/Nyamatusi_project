43da6116598a2e7784de3646e1689ab2c73d4c2d
Gamuchirai Muchada <117155740+x-ant-ie@users.noreply.github.com>
2023-02-17 09:41:32 +0200

    Add files via upload

---
+++ Bootstrap-Final-project/script.js
@@ -0,0 +1,23 @@
+
+
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+
+
+  /* Open */
+function openNav() {
+  document.getElementById("myNav").style.display ="block";
+  document.getElementById("myNav").style.height = "100%";
+
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+  // document.getElementById("myNav").style.display ="block";
+
+}
+
